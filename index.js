102ad303056ea8a9892d2e821ae50117124b62e6
Julius Te <mail@juliuste.de>
2017-06-08 04:54:30 +0200

    add simple logging, update dependencies, fix update script

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const compression  = require('compression')
 const nocache      = require('nocache')
 const path         = require('path')
 
+const morgan 	   = require('morgan')
+const shorthash    = require('shorthash').unique
+const p 		   = require('path')
+
 const impressum = require('./impressum')
 const route       = require('./route')
 
@@ -34,6 +38,14 @@ httpApi.set('forceSSLOptions', {
 })
 
 api.use(compression())
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(p.join(__dirname, 'access.log'), {flags: 'a'})
+
+// setup the logger
+morgan.token('id', (req, res) => req.headers['x-identifier'] || shorthash(req.ip))
+api.use(morgan(':date[iso] :id :method :url :status :response-time ms', {stream: accessLogStream}))
+
 
 api.use('/assets', express.static('assets'));
 
